d5fec0da929a403a1f7cf22a20a4cce441126e76
osmel1 <elhaoussama483@gmail.com>
2024-03-17 10:54:51 +0000

    first commit

--- src/main/java/com/example/tpspring1/controller/PatientController.java
+++ src/main/java/com/example/tpspring1/controller/PatientController.java
@@ -1,2 +1,55 @@
-package com.example.tpspring1.controller;public class PatientController {
+package com.example.tpspring1.controller;
+
+import com.example.tpspring1.models.Patient;
+import com.example.tpspring1.repository.PatientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepository patientRepository;
+    @GetMapping("/index")
+    public String index(Model model ,
+                        @RequestParam (name="page",defaultValue = "0") int page,
+                        @RequestParam (name="size",defaultValue = "5") int size,
+                        @RequestParam (name="keyword",defaultValue = "") String keyword
+    ){
+        //to get the list of patients with pagination and the keyword
+        Page<Patient> patients = patientRepository.findByNomContains(keyword, PageRequest.of(page, size));
+        model.addAttribute("patients", patients);
+        model.addAttribute("pages", new int[patients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
+        return "patients";
+    }
+
+    //on ajoute le keyword et currentPage pour garder les donnes apres la redirection
+    @GetMapping("/delete")
+    public String delete(Long id , String keyword , int page) {
+        System.out.println("you deleted the patient with id = "+id);
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/formPatient")
+    public String formPatient(Model model){
+        model.addAttribute("patient", new Patient());
+        model.addAttribute("mode", "new");
+        return "formPatient";
+    }
+    @PostMapping("/save")
+    public String save(Model model, Patient patient){
+        System.out.println(patient.getDateNaissance());
+        patientRepository.save(patient);
+        return "formPatient";
+    }
+
 }
--- src/main/java/com/example/tpspring1/models/Patient.java
+++ src/main/java/com/example/tpspring1/models/Patient.java
@@ -2,6 +2,7 @@ package com.example.tpspring1.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.management.ConstructorParameters;
 import java.util.Collection;
@@ -17,9 +18,11 @@ public class Patient {
     private Long id;
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
     private int score;
     @OneToMany(mappedBy = "patient")
     private Collection<RendezVous> rendezVous;
+
 }
